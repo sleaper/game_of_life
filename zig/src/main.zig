69b31df9d4891e9020def1fb36c340402d6f46f9
Petr Spac <petrik.spac@gmail.com>
2023-08-13 19:35:45 +0200

    feat: user input

---
+++ zig/src/main.zig
@@ -0,0 +1,25 @@
+const std = @import("std");
+
+pub fn main() !void {
+    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
+    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+
+    const size = try ask_user();
+
+    std.debug.print("Size is {}", .{size});
+}
+
+fn ask_user() !i64 {
+    const stdin = std.io.getStdIn().reader();
+    const stdout = std.io.getStdOut().writer();
+
+    var buf: [10]u8 = undefined;
+
+    try stdout.print("Select a size: ", .{});
+
+    if (try stdin.readUntilDelimiterOrEof(buf[0..], '\n')) |user_input| {
+        return std.fmt.parseInt(i64, user_input, 10);
+    } else {
+        return @as(i64, 0);
+    }
+}
