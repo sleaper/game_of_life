62974bb5da7b8cafe4333b1909d3be461123b297
Petr Spac <petrik.spac@gmail.com>
2023-08-14 20:12:29 +0200

    user input, 2d array

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -1,25 +1,80 @@
 const std = @import("std");
+const print = std.debug.print;
+
+const Cell = struct { live: bool };
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    print("All your {s} are belong to us.\n", .{"codebase"});
 
-    const size = try ask_user();
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
 
-    std.debug.print("Size is {}", .{size});
+    // Get user input
+    const width: u64 = try ask_for_number("Width: ");
+    const height: u64 = try ask_for_number("Height: ");
+    _ = try ask_for_string("Living cell symbol: ");
+
+    const matrix: [][]u64 = try allocator.alloc([]u64, height);
+
+    for (0..width) |i| {
+        const row: []u64 = try allocator.alloc(u64, width);
+        matrix[i] = row;
+    }
+
+    for (0..height) |x| {
+        for (0..width) |y| {
+            matrix[x][y] = 0;
+            print("{} ", .{matrix[x][y]});
+        }
+        print("\n", .{});
+    }
+
+    // printMatrix(testMat);
+
+    // var generation: u16 = 0;
+    //
+    // while (generation <= 100) {
+    //     generation += 1;
+    // }
+
+    std.debug.print("Width = {}, height = {}", .{ width, height });
+
+    // Dealloc matrix
+    for (matrix) |row| {
+        _ = allocator.free(row);
+    }
+    _ = allocator.free(matrix);
 }
 
-fn ask_user() !i64 {
+fn ask_for_number(str: []const u8) !u64 {
     const stdin = std.io.getStdIn().reader();
     const stdout = std.io.getStdOut().writer();
 
     var buf: [10]u8 = undefined;
 
-    try stdout.print("Select a size: ", .{});
+    print("{s}", .{str});
+    try stdout.print("", .{});
 
     if (try stdin.readUntilDelimiterOrEof(buf[0..], '\n')) |user_input| {
-        return std.fmt.parseInt(i64, user_input, 10);
+        print("Typeof {s}: {}\n", .{ user_input, @TypeOf(user_input) });
+        return std.fmt.parseInt(u64, user_input, 10);
     } else {
-        return @as(i64, 0);
+        return @as(u64, 0);
+    }
+}
+
+fn ask_for_string(str: []const u8) ![]u8 {
+    const stdin = std.io.getStdIn().reader();
+    const stdout = std.io.getStdOut().writer();
+
+    var buf: [10]u8 = undefined;
+
+    print("{s}", .{str});
+    try stdout.print("", .{});
+
+    if (try stdin.readUntilDelimiterOrEof(buf[0..], '\n')) |user_input| {
+        return user_input;
+    } else {
+        return null;
     }
 }
